85ce39c37a22eaa7213c86beb5e53fccc9511ac8
matheuscrok <matheusprogfut@outlook.com>
2022-05-09 20:09:07 -0300

    heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/app-fatto-task-front"));
+app.use(express.static(__dirname + "/dist/tasks"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/app-fatto-task-front/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/tasks/index.html"));
 });
 
 // Inicia a aplicação pela porta configurada
